cd32432aae4bc97f534d921e69bb1c3768ab75d1
Patricia Grings <patriciagrings@hotmail.com>
2024-08-02 17:39:02 -0300

    desenvolvimento exercício

--- desafios tabelas/03/tabela.js
+++ desafios tabelas/03/tabela.js
@@ -1,32 +1,44 @@
 var vetorNotas = []
+var vetorMedias = []
 var contNota = 1
 var contVet = 0
 var contMed = 0
 function substituirNotas() {
-    if (contNota <= 4) {    
+    
+    if (contNota <= 14) {    
         var notaInfo= window.prompt('Insira a nota: ')
-        var notaNum= Number.parseFloat(notaInfo)
-        vetorNotas.push(notaNum)
-        var n1Mat= document.getElementById('nota' + contNota)
-        n1Mat.innerText= vetorNotas[contVet]
-        /*window.alert(vetorNotas[contVet])*/
-
-        contNota ++
-        contVet ++
+        var notaNum= Number.parseFloat(notaInfo.replace(',','.'))
+            if (!isNaN(notaNum)) {   
+                vetorNotas.push(notaNum)
+                var n1Mat= document.getElementById('nota' + contNota)
+                n1Mat.innerText= vetorNotas[contVet].toFixed(2)
+                contNota ++
+                contVet ++
+            }
+            else {
+                window.alert('Insira uma nota válida!')
+            }
     }
-    if (contVet >= 4) {
-    for (var c=1;c<=3;c++){
-        window.alert(vetorNotas[contMed])
-        window.alert(vetorNotas[contMed+1])
-        var medNota= (vetorNotas[contMed]+vetorNotas[contMed+1])/2
-        var med= document.getElementById('notaMed' + c)
-        med.innerText= medNota
-        contMed= contMed+2
+    else {
+        window.alert ('Todas as notas já foram inseridas')
+    }
+    if (contVet >= 14) {
+        for (var c=1;c<=7;c++){
+            var medNota= (vetorNotas[contMed]+vetorNotas[contMed+1])/2
+            vetorMedias.push(medNota)
+            var med= document.getElementById('notaMed' + c)
+            med.innerText= medNota.toFixed(2)
+            contMed= contMed+2
+        }
+        var medExatas= (vetorMedias[0]+vetorMedias[1]+vetorMedias[2]+vetorMedias[3])/4
+        var medEx= document.getElementById('notaMedEx')
+        medEx.innerText= medExatas.toFixed(2)
+
+        var medHumanas= (vetorMedias[4]+vetorMedias[5]+vetorMedias[6])/3
+        var medHum= document.getElementById('notaMedHum')
+        medHum.innerText= medHumanas.toFixed(2)
+
+        document.getElementById('btnInserir').disabled = true
     }
 }
-}
 
-
-document.getElementById('notaForm').addEventListener('submit', function(event) {
-    event.preventDefault()
-})
