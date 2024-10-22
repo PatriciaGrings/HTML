7e0d826c6e1eebc0a7a9c933b3bd8fd04ffd1089
Patricia Grings <patriciagrings@hotmail.com>
2024-10-22 12:32:42 -0300

    desenvolvimento

---
+++ ex029/mq004/mq004.js
@@ -0,0 +1,10 @@
+var itensJs = document.getElementById('itens')
+
+function clickMenu() {
+    if (itensJs.style.display == 'block') {
+        itensJs.style.display = 'none'
+    }
+    else {
+        itensJs.style.display = 'block'
+    }
+}
